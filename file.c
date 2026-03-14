4ac38b9e58cb1ca9dfb6d8a218ff4083e903f0ea
Akshay Kumar <akshaykumar954012@gmail.com>
2026-03-14 18:42:36 +0530

    updated file.c

--- file.c
+++ file.c
@@ -7,31 +7,8 @@ typedef struct {
 }User;
 User usr[20];
 
-//void loginUser();
-void createUser() {
-    User usr;
-    printf("Enter a Username: ");
-    scanf("%29s",usr.username);
-    printf("Enter a Password: ");
-    scanf("%29s",usr.password);
-    FILE *fptr;
-    fptr = fopen("users.txt","a");
-    fprintf(fptr,"%s ",usr.username);
-    fprintf(fptr,"%s\n",usr.password);
-    fclose(fptr);
-}
-//void updateUser();
-//void deleteUser();
-
-//void createFile();
-//void writeFile();
-//void appendFile();
-//void showFile();
-//void listFile();
-
-int main() {
-    createUser();
-    /*printf("\n___Welcome_to_userFile_System___\n");
+/*void menu() {
+    printf("\n___Welcome_to_userFile_System___\n");
     int choice;
     int i = 0;
     while(i < 3) {
@@ -42,7 +19,6 @@ int main() {
         printf("5. Exit\n");
         printf("Enter a choice: ");
         scanf("%d",&choice);
-
         switch(choice) {
             case 1:
             loginUser();
@@ -58,6 +34,57 @@ int main() {
             printf("Invalid Input, please try to input a number from 1-5.\n");
         }
         i++;
-    }*/
+    }
+}*/
+
+//void loginUser();
+void createUser() {
+    User usr;
+    printf("Enter a Username: ");
+    scanf("%29s",usr.username);
+    printf("Enter a Password: ");
+    scanf("%29s",usr.password);
+    FILE *fptr_append;
+    fptr_append = fopen("users.txt","a");
+    fprintf(fptr_append,"%s ",usr.username);
+    fprintf(fptr_append,"%s\n",usr.password);
+    fclose(fptr_append);
+}
+
+void updateUser() {
+    User usr;
+    FILE *fptr_read;
+    fptr_read = fopen("users.txt","r");
+    char buffer[100];
+    char user[30];
+    printf("Enter your username: ");
+    scanf("%29s",user);
+    while(fscanf(fptr_read,"%s",buffer) != EOF) {
+        if(strcmp(user,buffer) != 0) {
+            FILE *fptr_append;
+            fptr_append = fopen("users.txt","a");
+            printf("Enter new username: ");
+            scanf("%29s",usr.username);
+            printf("Enter new password: ");
+            scanf("%29s",usr.password);
+            fprintf(fptr_append,"%s ",usr.username);
+            fprintf(fptr_append,"%s\n",usr.password);
+            fclose(fptr_append);
+            break;
+        }
+    }
+    fclose(fptr_read);
+}
+//void deleteUser();
+
+//void createFile();
+//void writeFile();
+//void appendFile();
+//void showFile();
+//void listFile();
+
+int main() {
+    //createUser();
+    updateUser();
     return 0;
 }
